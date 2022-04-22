dcbd840da83ca8eace779c9e74b7a35e6a36303e
Ivan.Sivukha <ivan.sivukha@uk-blu-mob-2528.stepstone.asa>
2022-04-22 12:08:48 +0100

    feat: enabling cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,21 @@
-import express, {Application} from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
-
-import { api } from './routes'
+// import cors from 'cors';
+import { api } from './routes';
 import { errorResponse } from './middleware/error';
 
 const app: Application = express();
 const port = 5002;
 
+// app.use(cors());
 app.use(bodyParser.json({ limit: '5MB' }));
 app.use(require('morgan')('dev'));
 
 app.use('/api', api);
 app.use(errorResponse);
 
-app.listen(port, () => console.log(`Backend server running on port ${port} and accessible on http://localhost:5002/api/v1/jobs`));
+app.listen(port, () =>
+  console.log(
+    `Backend server running on port ${port} and accessible on http://localhost:5002/api/v1/jobs`
+  )
+);
